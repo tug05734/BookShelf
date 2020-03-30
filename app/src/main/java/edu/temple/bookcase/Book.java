91e3920faaa73d146e9c824a0df35174ea0eaf51
Rajat Rajan <tug05734@temple.edu>
2020-03-30 16:10:25 -0400

    Added Book class with necessary fields

---
+++ app/src/main/java/edu/temple/bookcase/Book.java
@@ -0,0 +1,88 @@
+package edu.temple.bookcase;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable {
+
+    private int id, published;
+    private String title, author, coverURL;
+
+    public Book(int id, int published, String title, String author, String coverURL){
+        this.id = id;
+        this.published = published;
+        this.title = title;
+        this.author = author;
+        this.coverURL = coverURL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPublished() {
+        return published;
+    }
+
+    public void setPublished(int published) {
+        this.published = published;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCoverURL() {
+        return coverURL;
+    }
+
+    public void setCoverURL(String coverURL) {
+        this.coverURL = coverURL;
+    }
+
+    private Book(Parcel in){
+        id = in.readInt();
+        published = in.readInt();
+        title = in.readString();
+        author = in.readString();
+        coverURL = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
